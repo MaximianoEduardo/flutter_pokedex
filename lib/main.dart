33b138b63b9d1daa6740141401b7c3d156d16f80
Maximiano Eduardo <edu.maxi.96@gmail.com>
2022-08-05 15:11:43 -0300

    :art: Criando Estrutura

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pokedex',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: Container(),
     );
