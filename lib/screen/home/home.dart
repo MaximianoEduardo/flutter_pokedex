1f5b3d924f54c1336529bc36b33066970ee301d2
Maximiano Eduardo <edu.maxi.96@gmail.com>
2022-08-08 10:29:05 -0300

    :sparkles: Passando Arguments para Details

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/models/pokemon.dart';
+import 'package:flutter_pokedex/screen/datails/detail_container.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key, required this.list}) : super(key: key);
+  const HomePage({Key? key, required this.list, required this.onItemTab})
+      : super(key: key);
 
   final List<Pokemon> list;
+  final Function(String, DetailArguments) onItemTab;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +17,8 @@ class HomePage extends StatelessWidget {
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
               title: Text(list[index].name),
+              onTap: () => onItemTab(
+                  '/details', DetailArguments(name: list[index].name)),
             );
           }),
     );
--- lib/screen/home/home_container.dart
+++ lib/screen/home/home_container.dart
@@ -2,31 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/errors/failure.dart';
 import 'package:flutter_pokedex/models/pokemon.dart';
 import 'package:flutter_pokedex/repository/pokemon_repository.dart';
+import 'package:flutter_pokedex/screen/datails/detail_container.dart';
 import 'package:flutter_pokedex/screen/home/home.dart';
-import 'package:flutter_pokedex/screen/home/home_error.dart';
-import 'package:flutter_pokedex/screen/home/home_loading.dart';
+import 'package:flutter_pokedex/widgets/error.dart';
+import 'package:flutter_pokedex/widgets/loading.dart';
 
 class HomeContainer extends StatelessWidget {
-  const HomeContainer({Key? key, required this.repository}) : super(key: key);
+  const HomeContainer(
+      {Key? key, required this.repository, required this.onItemTap})
+      : super(key: key);
 
   final IPokemonRepository repository;
-
+  final Function(String, DetailArguments) onItemTap;
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Pokemon>>(
       future: repository.getAllPokemons(),
       builder: ((context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const HomeLoading();
+          return const Loading();
         }
 
         if (snapshot.connectionState == ConnectionState.done &&
             snapshot.hasData) {
-          return HomePage(list: snapshot.data!);
+          return HomePage(
+            list: snapshot.data!,
+            onItemTab: onItemTap,
+          );
         }
 
         if (snapshot.hasError) {
-          return HomeError(error: (snapshot.error as Failure).message!);
+          return Error(error: (snapshot.error as Failure).message!);
         }
 
         return Container();
