9ecd0a9d95a95a9f95bd242901f0f066d95068d7
Maximiano Eduardo <edu.maxi.96@gmail.com>
2022-08-08 10:29:23 -0300

    :sparkles: Recebendo Dados e criando Tela

---
+++ lib/screen/datails/detail_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../models/pokemon.dart';
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({Key? key, required this.name, required this.list})
+      : super(key: key);
+  final String name;
+  final List<Pokemon> list;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(children: [
+          SizedBox(
+            height: 400,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: list
+                    .map((e) => Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(e.name),
+                        ))
+                    .toList(),
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
