028029cfb1721279278fe710918425e4968c65aa
Maximiano Eduardo <edu.maxi.96@gmail.com>
2022-08-08 08:29:19 -0300

    :recycle: Passando HomeContainer e Dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_pokedex/screen/home/home.dart';
+import 'package:flutter_pokedex/repository/pokemon_repository.dart';
+import 'package:flutter_pokedex/screen/home/home_container.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pokedex',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const HomePage(
-        list: [],
-      ),
-    );
+        title: 'Pokedex',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: HomeContainer(repository: PokemonRepository(dio: Dio())));
   }
 }
