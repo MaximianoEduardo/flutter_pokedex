559d1709853a463e0765a01808cbe5f3db587899
Maximiano Eduardo <edu.maxi.96@gmail.com>
2022-08-08 08:30:19 -0300

    :sparkles: Tela Logica Aguardando Resposta da API

--- lib/screen/home/home_container.dart
+++ lib/screen/home/home_container.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/errors/failure.dart';
 import 'package:flutter_pokedex/models/pokemon.dart';
+import 'package:flutter_pokedex/repository/pokemon_repository.dart';
 import 'package:flutter_pokedex/screen/home/home.dart';
 import 'package:flutter_pokedex/screen/home/home_error.dart';
 import 'package:flutter_pokedex/screen/home/home_loading.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class HomeContainer extends StatelessWidget {
+  const HomeContainer({Key? key, required this.repository}) : super(key: key);
+
+  final IPokemonRepository repository;
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Pokemon>>(builder: ((context, snapshot) {
-      if (snapshot.connectionState == ConnectionState.waiting) {
-        return const HomeLoading();
-      }
+    return FutureBuilder<List<Pokemon>>(
+      future: repository.getAllPokemons(),
+      builder: ((context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const HomeLoading();
+        }
 
-      if (snapshot.connectionState == ConnectionState.done &&
-          snapshot.hasData) {
-        return HomePage(list: snapshot.data!);
-      }
+        if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.hasData) {
+          return HomePage(list: snapshot.data!);
+        }
 
-      if (snapshot.hasError) {
-        return HomeError(error: snapshot.error.toString());
-      }
+        if (snapshot.hasError) {
+          return HomeError(error: (snapshot.error as Failure).message!);
+        }
 
-      return Container();
-    }));
+        return Container();
+      }),
+    );
   }
 }
